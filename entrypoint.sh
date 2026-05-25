e74150edb42ba6e90866d18f27d02665919e40c3
krxshed <nkatashev@gmail.com>
2026-05-25 15:14:18 +0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-set -e
 
 python manage.py migrate
 
+echo "Creating superuser if not exists..."
 python manage.py shell <<EOF
 from django.contrib.auth import get_user_model
 User = get_user_model()
@@ -13,13 +13,19 @@ else:
     print("Superuser already exists")
 EOF
 
+echo "Checking if products need to be added..."
 python manage.py shell <<EOF
 from apps.products.models import Product
 if Product.objects.count() == 0:
-    exec(open('fill_db.py').read())
-    print("Products added")
-else:
-    print("Products already exist")
+    exit(1)
 EOF
+
+# Проверяем код возврата последней команды
+if [ $? -eq 1 ]; then
+    echo "Adding products..."
+    python fill_db.py
+else
+    echo "Products already exist"
+fi
 
 exec gunicorn coffee_shop.wsgi:application --bind 0.0.0.0:8000
