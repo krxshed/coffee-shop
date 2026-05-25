30885d7ea1b7427095c414e02925b842dbbcc95f
kroshed <nikita.katashev@mail.ru>
2026-05-25 14:11:15 +0500

    Fix entrypoint.sh: convert to LF line endings

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,7 @@
-﻿#!/bin/sh
+#!/bin/sh
 set -e
 
-
 python manage.py migrate
-
 
 python manage.py shell <<EOF
 from django.contrib.auth import get_user_model
@@ -15,12 +13,11 @@ else:
     print("Superuser already exists")
 EOF
 
-
 python manage.py shell <<EOF
 from apps.products.models import Product
 if Product.objects.count() == 0:
     exec(open('fill_db.py').read())
-    print("Products have been added to the database")
+    print("Products added")
 else:
     print("Products already exist")
 EOF
